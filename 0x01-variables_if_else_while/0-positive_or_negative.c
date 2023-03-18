09635d5ad555b0e37ac43bcf82a49517df278b24
meshack-abwao <meshack2141@gmail.com>
2023-03-18 00:24:23 +0000

    if else betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,9 +8,6 @@
  *
  * Return: Always 0 (Success)
  */
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
@@ -29,6 +26,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	/* your code goes there */
 	return (0);
 }
