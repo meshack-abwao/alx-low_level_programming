10246bfa4ecb312d257cad316f26dfffe12a3389
meshack-abwao <meshack2141@gmail.com>
2023-03-20 12:09:59 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point
+ * main - This is the main function
  *
  * return: always (0) success
  */
+
 int main(void)
 	{
 		printf("with proper grammar, but the outcome is a piece of art,\n");
