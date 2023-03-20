aeecf2039b464d3d5e11782810f6457f9a60a528
meshack-abwao <meshack2141@gmail.com>
2023-03-20 08:18:16 +0000

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**Main is the entrance
+ *
+ * prints the string with proper grammar, but the outcome is a piece of art, .
+ *
+ * uses the function printf
+ *
+ * return (0) success
+ */
+int main(void)
+	{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+	}
