16ccd8a410a43ead9ab07ca1a1af8f6aed118dc3
meshack-abwao <meshack2141@gmail.com>
2023-03-20 11:55:11 +0000

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - A program that prints a line using the printf function 
+ * main - A program that prints a line using the printf function
  *
  * return: always (0) success
  */
