fb2b638596a7799276fbd578f5c0d5b715704b14
meshack-abwao <meshack2141@gmail.com>
2023-03-20 11:45:37 +0000

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints the string with proper grammar, but the outcome is a piece of art,
+ * main - A program that prints with proper grammar, but the outcome is a piece of art,
  *
  * return: always (0) success
  */
