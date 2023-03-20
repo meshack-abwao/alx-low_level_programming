ca20d8d0666591d04934b0353fe6599d320f4a84
meshack-abwao <meshack2141@gmail.com>
2023-03-20 08:22:50 +0000

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * Main - is the entrance
+ * Main - Entry point
  *
  * prints the string with proper grammar, but the outcome is a piece of art, .
  *
