afa62f475c316a573584abe7ea5099dfd4e1a8ee
meshack-abwao <meshack2141@gmail.com>
2023-03-20 12:32:54 +0000

    sizeoff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,7 @@
 /**
  * main - Entry point
  *
- * prints the size of various types on the computer it is compiled and run on.
- *
- * return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
