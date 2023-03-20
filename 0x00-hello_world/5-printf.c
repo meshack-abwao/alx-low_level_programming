3b2160256f9f9c89f072ac085e60d5e2958df9bd
meshack-abwao <meshack2141@gmail.com>
2023-03-20 11:51:31 +0000

    hello print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point 
+ * main - A program that prints a line using the printf function 
  *
  * return: always (0) success
  */
 int main(void)
-/*a program that prints a string to the output*/
 	{
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
