dfe93d3353a3efc2cac9cb98a929dd51307e5855
meshack-abwao <meshack2141@gmail.com>
2023-03-20 07:10:07 +0000

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*A simple program that prints exactly the said text
+ *
+ * uses the function puts to do so
+ */
+int main()
+	{
+		puts("Programming is like building a multilingual puzzle\n");
+		return 0;
+	}
