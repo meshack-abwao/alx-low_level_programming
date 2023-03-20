e680346e0dd49b59ea46c7d649a59dec7b140865
meshack-abwao <meshack2141@gmail.com>
2023-03-20 10:21:10 +0000

    sizoff oeratios

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,14 @@
  * return: Always (0) success
  */
 int main(void)
+/* main - entry point*/
 {
 	printf("Size of a char: %ld byte(s) \n", sizeof(char));
 	printf("Size of an int: %ld byte(s) \n", sizeof(int));
-	printf("Size of a long: %ld byte(s) \n", sizeof(long));
-	printf("Size of a long long: %ld byte(s) \n", sizeof(long long));
+	printf("Size of a long int: %ld byte(s) \n", sizeof(long));
+	printf("Size of a long long int: %ld byte(s) \n", sizeof(long long));
 	printf("Size of a float: %ld byte(s) \n", sizeof(float));
-	return(0);
+
+	return (0);
+	/*return: always (0) success*/
 }
