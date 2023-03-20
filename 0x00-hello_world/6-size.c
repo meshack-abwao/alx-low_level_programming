e1ade0a15c25c05dd112c19b44a8705e88743e89
meshack-abwao <meshack2141@gmail.com>
2023-03-20 10:03:10 +0000

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Main - Entry point
+ *
+ * prints the string with proper grammar, but the outcome is a piece of art,
+ *
+ * return: (0) success
+ */
+int main(void)
+{
+	printf("the size of char is %ld \n", sizeof(char));
+	printf("the size of int is %ld \n", sizeof(int));
+	printf("the size of long is %ld \n", sizeof(long));
+	printf("the size of long long is %ld \n", sizeof(long long));
+	printf("the size of float is %ld \n", sizeof(float));
+	return(0);
+}
