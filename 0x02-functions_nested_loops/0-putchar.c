16047fbdd9f1b91d15a15d80ae18155a0a105d31
meshack-abwao <meshack2141@gmail.com>
2023-03-21 23:31:24 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
+}
+
