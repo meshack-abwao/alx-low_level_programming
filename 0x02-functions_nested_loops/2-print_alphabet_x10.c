8df08e391e1414ac8ae8bf2cc5244c515c3b81e5
meshack-abwao <meshack2141@gmail.com>
2023-03-22 08:27:29 +0000

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ *Return: Always 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int i;
+
+	for (i = 0; i <=10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar ('\n');
+	}
+}
