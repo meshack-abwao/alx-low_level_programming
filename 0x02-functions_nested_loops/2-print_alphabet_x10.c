1289a4e26586934bbd11e5d740185c6d3210dafb
meshack-abwao <meshack2141@gmail.com>
2023-03-22 09:04:21 +0000

    10x putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,21 +3,20 @@
 /**
  *  print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  *  followed by a new line
- *   
-*/
+ */
 
 void print_alphabet_x10(void)
 {
-	char alphabet;
+	char ch;
 	int i;
 
 	while (i <= 10)
 	{
-		alphabet = 'a';
-		while (alphabet <= 'z')
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(alphabet);
-			alphabet++;
+			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
 		i++;
