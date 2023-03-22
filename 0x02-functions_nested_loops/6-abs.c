1ff9fd0ec65a2312b58b9d714d18f4750e214dea
meshack-abwao <meshack2141@gmail.com>
2023-03-22 18:38:31 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Returns: Always 0 (success)
+ */
+int _abs(int)
+{
+	if (int < 0)
+		return (-int);
+	else
+		return (int);
+}
