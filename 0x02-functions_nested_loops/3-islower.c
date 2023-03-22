7ef6cfdf7716ea02973a60e0796bce1006acaf01
meshack-abwao <meshack2141@gmail.com>
2023-03-22 11:50:32 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,9 @@
  * main - Entry point
  *
  * _islower checks if a character (c) is lower case
+ * @c is the character to be checked
  *
- * Return: Always 1 (success)
+ * Return: Always 1 (success) else 0
  */
 
 int _islower(int c)
