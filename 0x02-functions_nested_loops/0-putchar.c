0e6b32c27e93991c2fed1d97b85459401a6c73d2
meshack-abwao <meshack2141@gmail.com>
2023-03-22 00:21:39 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-        _putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -19,6 +19,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-        return (0);
+	return (0);
 }
 
