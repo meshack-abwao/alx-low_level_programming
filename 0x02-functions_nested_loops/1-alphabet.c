f33a28224b35ae0cc2376f7965635985d39073be
meshack-abwao <meshack2141@gmail.com>
2023-03-22 00:40:04 +0000

    for alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Prints alphabet in lowercase
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+	{
+		char c;
+
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+
+		return 0;
+	}
+
