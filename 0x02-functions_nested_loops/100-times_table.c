333ba80430d6728b791f6f74625be167f4d7f149
meshack-abwao <meshack2141@gmail.com>
2023-03-22 23:52:10 +0000

    timetables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,22 +11,22 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i =0; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j + 0; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				k = j * 1;
 				if (j == 0)
 				{
 					_putchar(k + '0');
 				}
-				else if (k <10 && j != 0)
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(k + ;0);
+					_putchar(k + 0);
 				}
 				{
 					else if (k >= 10 && k < 100)
@@ -46,7 +46,7 @@ void print_times_table(int n)
 						_putchar((k % 10) + '0');
 					}
 				}
-				_putchar('\n);
+				_putchar('\n');
 			}
 		}
 	}
