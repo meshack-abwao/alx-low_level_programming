796368c8ab5c805c66dfadd8398d23b2142cef47
meshack-abwao <meshack2141@gmail.com>
2023-03-27 20:54:53 +0000

    more pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,12 +3,12 @@
 /**
  *swap_int - swap the values of two integers
  * @a: integer to be swapped
- * 2b: integer to swap
+ * @b: integer to swap
 */
 void swap_int(int *a, int *b)
 {
 	int i;
-	
+
 	i = *a;
 	*a = *b;
 	*b = i;
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 /**
  * _strlen - returns the length of a string
  * @s: string
